6cb4788aa211bf6d12db7b1b654ea1212b61ea38
shaimeur <76816609+shaimeur@users.noreply.github.com>
2023-01-03 01:58:04 +0100

    ==> task task-item componenet
    adding new components as mentioned in the tittle

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent implements OnInit{
-  @Input()  text  : string = "Add";
-  @Input()  color : string = "red";
+  @Input()  text  ?: string ;
+  @Input()  color ?: string ;
   @Output() btnOnClick = new EventEmitter();
   constructor() {}
 
