7f1c07b54dc60abcf72c72529ff23bfc06d51bd6
shaimeur <76816609+shaimeur@users.noreply.github.com>
2023-01-04 19:27:24 +0100

    adding the delete method
    adding the onDelete Event && the delete Method

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit,Input } from '@angular/core';
+import { Component,OnInit,Input,Output,EventEmitter } from '@angular/core';
 import { Task } from '../../Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,10 +8,12 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./task-item.component.css']
 })
 export class TaskItemComponent implements OnInit{
-    @Input() task ?: any
     faTimes = faTimes;
+    @Input() task : Task ;
+    @Output() onDeleteTask:EventEmitter<Task> = new EventEmitter();
     constructor() {}
-    ngOnInit(): void {
-
-    }
+    ngOnInit(): void {}
+    onDelete(task){
+      this.onDeleteTask.emit(task);
+   }
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -10,9 +10,13 @@ export class TasksComponent implements OnInit{
     tasks : Task[] = [];
     constructor(private taskService : TaskService) {}
     ngOnInit(): void {
-        this.taskService.getTasks().subscribe((tasks) =>{
+          this.taskService.getTasks().subscribe((tasks) =>{
           this.tasks = tasks
         })
     }
-
+    deleteTask(task : Task){
+      this.taskService.deleteTask(task).subscribe(() =>(
+        this.tasks = this.tasks.filter(t => t.id  !== task.id)
+      ));
+    }
 }
